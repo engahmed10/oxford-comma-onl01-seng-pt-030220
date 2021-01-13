4ecc987b683d7e3001b235aa45bb36d4b9112d41
AHMED AFANDY <eng.computer1@yahoo.com>
2021-01-13 13:23:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
     elsif  array.length > 2
       "#{array[0..array.length-2].join(", ")}, and #{array[array.length-1]}"
     else
-      array.join
+          array.join
     end
-  
+
 end
